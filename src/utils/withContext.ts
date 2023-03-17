fdd02f82b24f2227fa8d67722ff86ba831bb1d21
bigmistqke <vandijckv@gmail.com>
2023-03-17 21:48:04 +0100

    withContext with overload

--- src/utils/withContext.ts
+++ src/utils/withContext.ts
@@ -1,17 +1,60 @@
 import { Accessor, Context, JSX, untrack } from 'solid-js'
 
-export default function withContext<T>(
-  children: Accessor<JSX.Element | JSX.Element[]>,
-  context: Context<T>,
-  value: T,
-) {
-  let result: JSX.Element | JSX.Element[]
+type CheckSingle<Arg> = Arg extends { context: infer TContext; value: infer TValue }
+  ? TContext extends Context<TValue | undefined>
+    ? true
+    : false
+  : false
+
+type CheckContexts<Array> = Array extends (infer Arg)[]
+  ? CheckSingle<Arg> extends true
+    ? Array
+    : false
+  : false
+
+const nestContexts = (contexts: any, index: number, callback: () => void) => {
+  const { context, value } = contexts[index]!
   context.Provider({
     value: untrack(() => value),
     children: () => {
-      result = children()
+      if (index < contexts.length - 1) {
+        nestContexts(contexts, index + 1, callback)
+      } else {
+        callback()
+      }
       return ''
     },
   })
+}
+
+function withContext<T extends [...any[]]>(
+  children: Accessor<JSX.Element | JSX.Element[]>,
+  contexts: CheckContexts<T>,
+): Accessor<JSX.Element | JSX.Element[]>
+function withContext<T>(
+  children: Accessor<JSX.Element | JSX.Element[]>,
+  context: Context<T>,
+  value: T,
+): Accessor<JSX.Element | JSX.Element[]>
+function withContext<T extends [...any[]]>(
+  children: Accessor<JSX.Element | JSX.Element[]>,
+  contextOrContexts: Context<T> | CheckContexts<T>,
+  value?: T,
+) {
+  let result: JSX.Element | JSX.Element[]
+
+  if (Array.isArray(contextOrContexts)) {
+    nestContexts(contextOrContexts, 0, () => (result = children()))
+  } else if (typeof contextOrContexts === 'object' && value) {
+    contextOrContexts.Provider({
+      value: untrack(() => value),
+      children: () => {
+        result = children()
+        return ''
+      },
+    })
+  }
+
   return () => result
 }
+export default withContext
