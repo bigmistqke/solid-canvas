0cacb31ef0b9fc0e8e49800633ea59a79b61ee1d
bigmistqke <vandijckv@gmail.com>
2023-03-22 00:11:46 +0100

    add examples

---
+++ dev/pages/CubicsColored.tsx
@@ -0,0 +1,125 @@
+import { Component, createMemo, createSignal, For, on, Show } from 'solid-js'
+import { Bezier, Canvas, Group, Quadratic, QuadraticPoints } from 'src'
+
+const RandomBezier = (props: {
+  counter: number
+  scale: number
+  cursor: { x: number; y: number }
+}) => {
+  const getPoints = () => {
+    const points = []
+    let i = 0
+    const amount = 10
+    const variation = 500
+    while (i <= amount) {
+      const point =
+        i === 0 || i === amount
+          ? {
+              x: (i * window.innerWidth) / amount,
+              y: window.innerHeight,
+            }
+          : {
+              x: (i * window.innerWidth) / amount,
+              y:
+                i % 2 === 0
+                  ? window.innerHeight / 2 + Math.random() * variation - variation / 2
+                  : window.innerHeight / 3 -
+                    (window.innerHeight / 3) * props.scale +
+                    Math.random() * variation,
+            }
+      const control =
+        i === 0 || i === amount
+          ? {
+              x: 0,
+              y: -50,
+            }
+          : {
+              x: -50,
+              y: 0,
+            }
+      points.push({
+        point,
+        control,
+      })
+      i++
+    }
+    return points
+  }
+
+  const fill = createMemo(
+    on(
+      () => props.counter,
+      () => {
+        return {
+          h: Math.random() * 360,
+          s: Math.random() * 50 + 25,
+          l: Math.random() * 50 + 25,
+        }
+      },
+    ),
+  )
+
+  const points = createMemo(
+    on(
+      () => props.counter,
+      () => getPoints(),
+    ),
+  )
+
+  return (
+    <Bezier
+      fill={{
+        l: fill().l,
+        s: fill().s * (props.cursor.y / window.innerHeight),
+        h: (fill().h + (360 * props.cursor.x) / window.innerWidth) % 360,
+      }}
+      points={points()}
+      lineWidth={2}
+      stroke="transparent"
+      close
+      composite={'soft-light'}
+    />
+  )
+}
+
+const App: Component = () => {
+  const [counter, setCounter] = createSignal(0)
+  const [debug, setDebug] = createSignal(false)
+  const increment = () => setCounter(c => c + 1)
+  window.addEventListener('keydown', event => {
+    if (event.code === 'Space') increment()
+  })
+  window.addEventListener('resize', event => {
+    increment()
+  })
+  const amount = 5
+
+  const randomFill = createMemo(
+    on(counter, () => {
+      return `rgb(${Math.random() * 200}, ${Math.random() * 200}, ${Math.random() * 200})`
+    }),
+  )
+  const [cursor, setCursor] = createSignal({ x: 0, y: 0 })
+  window.addEventListener('mousemove', e => setCursor({ x: e.clientX, y: e.clientY }))
+  return (
+    <>
+      <Canvas
+        style={{ width: '100%', height: '100%', background: randomFill() }}
+        alpha
+        // stats
+        onMouseDown={increment}
+        debug={debug()}
+      >
+        <Group position={{ x: 0, y: 0 }}>
+          <For each={new Array(amount).fill('')}>
+            {(_, i) => (
+              <RandomBezier counter={counter()} scale={(amount - i()) / amount} cursor={cursor()} />
+            )}
+          </For>
+        </Group>
+      </Canvas>
+    </>
+  )
+}
+
+export default App
---
+++ dev/pages/Smileys.tsx
@@ -0,0 +1,73 @@
+import { Component, createMemo, createSignal, For, on } from 'solid-js'
+import { Arc, Bezier, Canvas, Group } from 'src'
+
+const Smiley = (props: { counter: number }) => {
+  const [offset, setOffset] = createSignal({ x: 0, y: 0 })
+  const randomPosition = {
+    x: Math.random() * (window.innerWidth + 100) - 50,
+    y: Math.random() * (window.innerHeight + 100) - 50,
+  }
+  const delta = Math.random() * 10 + 20
+
+  const position = createMemo(
+    on(
+      () => props.counter,
+      () => ({
+        x: randomPosition.x + Math.sin(performance.now() / (20 * delta) + delta) * 20,
+        y: randomPosition.y + Math.cos(performance.now() / (20 * delta) + delta) * 20,
+      }),
+    ),
+  )
+
+  const color = createMemo(() => ({ h: Math.random() * 360, s: 50, l: 50 }))
+  return (
+    <Arc
+      position={{ x: position().x, y: position().y }}
+      radius={50}
+      fill={color()}
+      stroke="transparent"
+      draggable
+    >
+      <Group position={{ x: 0, y: 25 }}>
+        <Arc position={{ x: 20, y: 0 }} radius={5} fill="black" />
+        <Arc position={{ x: 70, y: 0 }} radius={5} fill="black" />
+      </Group>
+      <Group position={{ x: 0, y: 60 }}>
+        <Bezier
+          lineWidth={5}
+          // shadow={{ blur: 0, offset: { x: 5, y: 5 }, color: 'black' }}
+          points={[
+            { point: { x: 25, y: 0 }, control: { x: 0, y: 15 } },
+            { point: { x: 50, y: 25 }, control: { x: -15, y: 0 } },
+            { point: { x: 75, y: 0 }, control: { x: 0, y: 15 } },
+          ]}
+        />
+      </Group>
+    </Arc>
+  )
+}
+
+const App: Component = () => {
+  const [counter, setCounter] = createSignal(0)
+  const increment = () => setCounter(c => c + 1)
+  window.addEventListener('keydown', event => {
+    if (event.code === 'Space') increment()
+  })
+  window.addEventListener('resize', increment)
+
+  const fill = `rgb(${Math.random() * 200}, ${Math.random() * 200}, ${Math.random() * 200})`
+  const loop = () => {
+    requestAnimationFrame(loop)
+    increment()
+  }
+  loop()
+  return (
+    <>
+      <Canvas style={{ width: '100%', height: '100%', fill }} alpha stats onMouseDown={increment}>
+        <For each={new Array(100).fill('')}>{() => <Smiley counter={counter()} />}</For>
+      </Canvas>
+    </>
+  )
+}
+
+export default App
