7ec038e713f4a1049e90af64a3ebb283903cb226
bigmistqke <vandijckv@gmail.com>
2023-03-21 14:55:27 +0100

    export `type QuadraticPoints`

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ export { Image } from './components/Shape/Image'
 
 export { Line } from './components/Shape/Path2D/Line'
 export { Bezier } from './components/Shape/Path2D/Bezier'
-export { Quadratic } from './components/Shape/Path2D/Quadratic'
+export { Quadratic, type QuadraticPoints } from './components/Shape/Path2D/Quadratic'
 
 export { Rectangle } from './components/Shape/Path2D/Rectangle'
 export { Arc } from './components/Shape/Path2D/Arc'
