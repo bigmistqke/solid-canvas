cfc900bf2ff9bda075b2f3bc6a5e8587ff3abd14
bigmistqke <vandijckv@gmail.com>
2023-03-22 20:11:13 +0100

    refactor `Text` to use `Rectangle` as background

--- src/components/Shape/Path2D/Rectangle.tsx
+++ src/components/Shape/Path2D/Rectangle.tsx
@@ -1,5 +1,5 @@
 import { createToken } from '@solid-primitives/jsx-tokenizer'
-import { mergeProps } from 'solid-js'
+import { createEffect, createSignal, mergeProps } from 'solid-js'
 
 import { useInternalContext } from 'src/context/InternalContext'
 import { parser, ShapeToken } from 'src/parser'
@@ -13,78 +13,85 @@ import useDraggable from 'src/utils/useDraggable'
 import useMatrix from 'src/utils/useMatrix'
 import withGroup from 'src/utils/withGroup'
 
+export type RectangleProps = Normalize<
+  ShapeProps & {
+    dimensions: Dimensions
+    onMouseEnter?: () => void
+    onMouseLeave?: () => void
+    rounded?:
+      | number
+      | [all: number]
+      | [topLeftAndBottomRight: number, topRightAndBottomLeft: number]
+      | [topLeft: number, topRightAndBottomLeft: number, bottomRight: number]
+  }
+>
+
 /**
  * Paints a rectangle to the canvas
  * [link](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/rect)
  */
 
-const Rectangle = createToken(
-  parser,
-  (
-    props: Normalize<
-      ShapeProps & {
-        dimensions: Dimensions
-        rounded?:
-          | number
-          | [all: number]
-          | [topLeftAndBottomRight: number, topRightAndBottomLeft: number]
-          | [topLeft: number, topRightAndBottomLeft: number, bottomRight: number]
-      }
-    >,
-  ) => {
-    const canvas = useInternalContext()
-    const merged = mergeProps({ ...defaultShapeProps, close: true }, props)
-    const [dragPosition, dragEventHandler] = useDraggable()
+const Rectangle = createToken(parser, (props: RectangleProps) => {
+  const canvas = useInternalContext()
+  const merged = mergeProps({ ...defaultShapeProps, close: true }, props)
+  const [dragPosition, dragEventHandler] = useDraggable()
 
-    const matrix = useMatrix(merged, dragPosition)
+  const matrix = useMatrix(merged, dragPosition)
 
-    const getPath = () => {
-      const path = new Path2D()
-      if (props.rounded)
-        path.roundRect(0, 0, merged.dimensions.width, merged.dimensions.height, props.rounded)
-      else path.rect(0, 0, merged.dimensions.width, merged.dimensions.height)
-      return path
-    }
+  const getPath = () => {
+    const path = new Path2D()
+    if (props.rounded && 'roundRect' in path)
+      path.roundRect(0, 0, merged.dimensions.width, merged.dimensions.height, props.rounded)
+    else path.rect(0, 0, merged.dimensions.width, merged.dimensions.height)
+    return path
+  }
 
-    const path = transformPath(getPath, matrix)
+  const path = transformPath(getPath, matrix)
 
-    const bounds = useBounds(
-      () => [
-        {
-          x: 0,
-          y: 0,
-        },
-        {
-          x: props.dimensions.width,
-          y: 0,
-        },
-        {
-          x: props.dimensions.width,
-          y: props.dimensions.height,
-        },
-        {
-          x: 0,
-          y: props.dimensions.height,
-        },
-      ],
-      matrix,
-    )
-
-    return {
-      id: 'Rectangle',
-      type: 'Shape',
-      render: (ctx: CanvasRenderingContext2D) => {
-        renderPath(ctx, merged, path())
+  const bounds = useBounds(
+    () => [
+      {
+        x: 0,
+        y: 0,
       },
-      debug: (ctx: CanvasRenderingContext2D) => renderPath(ctx, defaultBoundsProps, bounds().path),
-      path,
-      hitTest: function (event) {
-        const token: ShapeToken = this
-        return hitTest(token, event, canvas?.ctx, merged, dragEventHandler)
+      {
+        x: props.dimensions.width,
+        y: 0,
       },
-    }
-  },
-)
+      {
+        x: props.dimensions.width,
+        y: props.dimensions.height,
+      },
+      {
+        x: 0,
+        y: props.dimensions.height,
+      },
+    ],
+    matrix,
+  )
+
+  const [hover, setHover] = createSignal(false)
+  createEffect(() => {
+    if (hover()) props.onMouseEnter?.()
+    else props.onMouseLeave?.()
+  })
+
+  return {
+    id: 'Rectangle',
+    type: 'Shape',
+    render: (ctx: CanvasRenderingContext2D) => {
+      renderPath(ctx, merged, path())
+    },
+    debug: (ctx: CanvasRenderingContext2D) => renderPath(ctx, defaultBoundsProps, bounds().path),
+    path,
+    hitTest: function (event) {
+      const token: ShapeToken = this
+      const result = hitTest(token, event, canvas?.ctx, merged, dragEventHandler)
+      setHover(result)
+      return result
+    },
+  }
+})
 
 const GroupedRectangle = withGroup(Rectangle)
 
---
+++ src/components/Shape/Text.tsx
@@ -0,0 +1,155 @@
+import { createToken, TokenComponent } from '@solid-primitives/jsx-tokenizer'
+import { createEffect, createMemo, createSignal, mergeProps, Show, splitProps } from 'solid-js'
+import { Rectangle } from 'src'
+
+import { useInternalContext } from 'src/context/InternalContext'
+import { parser } from 'src/parser'
+import { Dimensions, ExtendedColor, Normalize, ShapeProps } from 'src/types'
+import { defaultShapeProps } from 'src/utils/defaultProps'
+import filterShapeProps from 'src/utils/filterShapeProps'
+import { resolveExtendedColor } from 'src/utils/resolveColor'
+import { GroupProps } from '../Group'
+import { RectangleProps } from './Path2D/Rectangle'
+
+type Rounded =
+  | number
+  | [all: number]
+  | [topLeftAndBottomRight: number, topRightAndBottomLeft: number]
+  | [topLeft: number, topRightAndBottomLeft: number, bottomRight: number]
+
+type TextProps = Normalize<
+  ShapeProps & {
+    text: string
+    size?: number
+    fontFamily?: string
+    background?: ExtendedColor
+    dimensions: Dimensions
+    padding?: number
+    /**
+     * Currently not yet supported in firefox [link](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/roundRect#browser_compatibility)
+     */
+    rounded?: Rounded
+  }
+>
+
+const getFontString = (size?: number, fontFamily?: string) =>
+  `${size ?? 10}pt ${fontFamily ?? 'Arial'}`
+
+/**
+ * Paints a text to the canvas
+ * [link](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/fillText)
+ */
+
+const Text = createToken(parser, (props: TextProps) => {
+  const canvas = useInternalContext()
+  const merged = mergeProps(
+    {
+      ...defaultShapeProps,
+      close: true,
+      fontFamily: 'arial',
+      size: 10,
+      fill: 'black',
+      stroke: 'transparent',
+      padding: 0,
+    },
+    props,
+  )
+  const filteredProps = filterShapeProps(merged)
+
+  const render = (ctx: CanvasRenderingContext2D) => {
+    const offset = canvas?.origin ?? { x: 0, y: 0 }
+    ctx.font = getFontString(props.size, props.fontFamily)
+
+    if (props.opacity) ctx.globalAlpha = props.opacity
+    ctx.fillStyle = resolveExtendedColor(merged.fill) ?? 'black'
+    ctx.strokeStyle = resolveExtendedColor(merged.stroke) ?? 'transparent'
+
+    // TODO:  optimization: render text to OffscreenCanvas instead of re-rendering each frame
+    if (ctx.fillStyle !== 'transparent')
+      ctx.fillText(
+        merged.text,
+        merged.position.x + offset.x,
+        merged.position.y + offset.y + props.dimensions.height,
+      )
+    if (ctx.strokeStyle !== 'transparent')
+      ctx.strokeText(
+        merged.text,
+        merged.position.x + offset.x,
+        merged.position.y + offset.y + props.dimensions.height,
+      )
+  }
+
+  return {
+    props: filteredProps,
+    type: 'Shape',
+    id: 'Text',
+    render,
+  }
+})
+
+function withRectangle<T extends TextProps, U extends unknown>(Component: TokenComponent<T, U>) {
+  return (
+    props: Omit<T, 'dimensions'> &
+      Omit<RectangleProps, 'dimensions'> &
+      GroupProps & {
+        hover?: Omit<RectangleProps, 'dimensions'> & { background: ExtendedColor }
+      },
+  ) => {
+    const [hover, setHover] = createSignal(false)
+    const canvas = useInternalContext()
+
+    const [rectangleProps, textProps] = splitProps(props, [
+      'rounded',
+      'position',
+      'clip',
+      'onMouseDown',
+      'onMouseMove',
+      'onMouseUp',
+    ])
+
+    const [dimensions, setDimensions] = createSignal<Dimensions>()
+
+    createEffect(() => {
+      if (!canvas) return
+      // TODO:  this timeout is needed in chrome (else all metrics would be 0)
+      setTimeout(() => {
+        canvas.ctx.font = getFontString(props.size, props.fontFamily)
+        const metrics = canvas.ctx.measureText(props.text)
+        setDimensions({
+          width: metrics.width,
+          height: metrics.actualBoundingBoxAscent,
+        })
+      }, 0)
+    })
+
+    const textPropsWithHover = createMemo(() =>
+      hover() ? { ...textProps, ...textProps.hover } : textProps,
+    )
+
+    return (
+      <Show when={dimensions()}>
+        <Rectangle
+          {...rectangleProps}
+          position={rectangleProps.position}
+          lineWidth={textProps.padding}
+          fill={textPropsWithHover().background}
+          stroke={textPropsWithHover().background}
+          dimensions={dimensions()!}
+          onMouseEnter={() => setHover(true)}
+          onMouseLeave={() => setHover(false)}
+          composite={props.composite}
+        >
+          {props.children}
+          <Component
+            {...(textPropsWithHover() as T)}
+            composite={props.composite}
+            dimensions={dimensions()!}
+          />
+        </Rectangle>
+      </Show>
+    )
+  }
+}
+
+const TextWithRectangle = withRectangle(Text)
+export { TextWithRectangle as Text }
