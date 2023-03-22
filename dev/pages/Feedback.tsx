913324566222f837f218b10c9b75471ad954a900
bigmistqke <vandijckv@gmail.com>
2023-03-22 16:39:22 +0100

    add examples

---
+++ dev/pages/Feedback.tsx
@@ -0,0 +1,159 @@
+import { Component, createMemo, createSignal, on, Show } from 'solid-js'
+import { Arc, Canvas, useCanvas, useClock } from 'src'
+import { Composite, Position } from 'src/types'
+
+const randomColor = (alpha?: number) => ({
+  h: Math.random() * 360,
+  s: 50,
+  l: 50,
+  a: alpha ?? 0.5,
+})
+
+const Dot = (props: { switchColor: number; position: Position; switchScene: number }) => {
+  const context = useCanvas()
+  const [position, setPosition] = createSignal({ x: 0, y: 0 })
+
+  const delta = Math.random() * 10 + 20
+
+  context?.onFrame(() => {
+    setPosition({
+      x: props.position.x - radius() + Math.sin(performance.now() / (10 * delta) + delta) * 10,
+      y: props.position.y - radius() + Math.cos(performance.now() / (10 * delta) + delta) * 10,
+    })
+  })
+
+  const fill = createMemo(
+    on(
+      () => props.switchColor,
+      () => randomColor(Math.random() + 0.2),
+    ),
+  )
+
+  const radius = createMemo(
+    on(
+      () => props.switchScene,
+      () => 50 * Math.random() * 2 + 25,
+    ),
+  )
+  return (
+    <Arc
+      position={{ x: position().x, y: position().y }}
+      radius={radius()}
+      fill={fill()}
+      stroke="transparent"
+      draggable
+      pointerEvents={false}
+    />
+  )
+}
+
+const fxs: {
+  filter: string
+  alpha: number
+  composite?: Composite
+  offset?: { x: number; y: number } | (() => { x: number; y: number })
+  double?: boolean
+}[] = [
+  {
+    filter: ``,
+    alpha: 0.9,
+    composite: 'source-over',
+  },
+  {
+    filter: `hue-rotate(10deg) brightness(101%)`,
+    alpha: 0.1,
+    composite: 'source-over',
+    offset: {
+      x: 2,
+      y: 2,
+    },
+  },
+  {
+    filter: ``,
+    alpha: 0.1,
+    offset: {
+      x: 0,
+      y: -5,
+    },
+    composite: 'lighten',
+  },
+  {
+    filter: ``,
+    alpha: 0.75,
+    offset: () => ({
+      x: -1 * Math.sin(performance.now() / 5000),
+      y: -1 * Math.cos(performance.now() / 5000),
+    }),
+    composite: 'source-over',
+    double: true,
+  },
+]
+
+const App: Component = () => {
+  const [switchScene, setSwitchScene] = createSignal(0)
+  const [cursor, setCursor] = createSignal<{ x: number; y: number }>()
+  const fill = randomColor(1)
+
+  const clock = useClock()
+  clock.start()
+
+  const switchColor = createMemo(() => Math.floor(clock.clock() / 200))
+
+  const incrementSwitchScene = () => setSwitchScene(c => (c + 1) % fxs.length)
+
+  window.addEventListener('keydown', event => {
+    if (event.code === 'Space') {
+      event.preventDefault()
+      incrementSwitchScene()
+    }
+  })
+
+  const feedback = (ctx: CanvasRenderingContext2D) => {
+    const bitmap = createImageBitmap(ctx.canvas)
+    bitmap.then(bitmap => {
+      const fx = fxs[switchScene()]
+      ctx.restore()
+      ctx.save()
+      ctx.globalAlpha = fx?.alpha ?? 1
+      if (fx?.composite) ctx.globalCompositeOperation = fx?.composite
+
+      ctx.filter = fx?.filter ?? ''
+
+      const offset = typeof fx?.offset === 'function' ? fx?.offset() : fx?.offset
+      ctx.drawImage(bitmap, offset?.x ?? 0, offset?.y ?? 0, ctx.canvas.width, ctx.canvas.height)
+      if (fx?.double) {
+        ctx.globalAlpha = 0.25
+        ctx.drawImage(
+          bitmap,
+          (offset?.x ?? 0) * 5,
+          (offset?.y ?? 0) * 5,
+          ctx.canvas.width,
+          ctx.canvas.height,
+        )
+      }
+      ctx.restore()
+      bitmap.close()
+    })
+  }
+
+  return (
+    <>
+      <Canvas
+        fill={fill}
+        clock={clock.clock()}
+        style={{ width: '100%', height: '100%' }}
+        alpha
+        feedback={feedback}
+        cursor="crosshair"
+        onMouseMove={event => setCursor(event.position)}
+        onMouseDown={event => incrementSwitchScene()}
+      >
+        <Show when={cursor()}>
+          <Dot switchColor={switchColor()} switchScene={switchScene()} position={cursor()!} />
+        </Show>
+      </Canvas>
+    </>
+  )
+}
+
+export default App
---
+++ dev/pages/SmileyPaint.tsx
@@ -0,0 +1,117 @@
+import { Component, createEffect, createMemo, createSignal, For, on, Show } from 'solid-js'
+import { Arc, Bezier, Canvas, Group, useCanvas, useClock } from 'src'
+import { Position } from 'src/types'
+
+const randomColor = (alpha?: number) => ({ h: Math.random() * 360, s: 50, l: 50, a: alpha ?? 0.5 })
+
+const Smiley = (props: { counter: number; position: Position }) => {
+  const context = useCanvas()
+  const [position, setPosition] = createSignal({ x: 0, y: 0 })
+
+  const delta = Math.random() * 10 + 20
+
+  context?.onFrame(() => {
+    setPosition({
+      x: props.position.x - 50 * scale + Math.sin(performance.now() / (20 * delta) + delta) * 20,
+      y: props.position.y - 50 * scale + Math.cos(performance.now() / (20 * delta) + delta) * 20,
+    })
+  })
+
+  const fill = createMemo(
+    on(
+      () => props.counter,
+      () => randomColor(0.35),
+    ),
+  )
+
+  const scale = 2
+  return (
+    <Arc
+      position={{ x: position().x, y: position().y }}
+      radius={50 * scale}
+      fill={fill()}
+      stroke="transparent"
+      draggable
+      pointerEvents={false}
+    >
+      <Group position={{ x: 0, y: 25 * scale }}>
+        <Arc
+          position={{ x: 20 * scale, y: 0 }}
+          radius={5 * scale}
+          fill="black"
+          stroke="transparent"
+        />
+        <Arc
+          position={{ x: 70 * scale, y: 0 }}
+          radius={5 * scale}
+          fill="black"
+          stroke="transparent"
+        />
+      </Group>
+      <Group position={{ x: 0, y: 60 * scale }}>
+        <Bezier
+          lineCap="round"
+          lineWidth={5 * scale}
+          stroke="black"
+          points={[
+            { point: { x: 25 * scale, y: 0 }, control: { x: 0, y: 15 * scale } },
+            { point: { x: 50 * scale, y: 25 * scale }, control: { x: -15 * scale, y: 0 } },
+            { point: { x: 75 * scale, y: 0 }, control: { x: 0, y: 15 * scale } },
+          ]}
+        />
+      </Group>
+    </Arc>
+  )
+}
+
+const App: Component = () => {
+  const [counter, setCounter] = createSignal(0)
+  const increment = () => setCounter(c => c + 1)
+  window.addEventListener('keydown', event => {
+    if (event.code === 'Space') increment()
+  })
+  window.addEventListener('resize', event => {
+    increment()
+  })
+
+  const fill = createMemo(
+    on(
+      counter,
+      () => `rgb(${Math.random() * 200}, ${Math.random() * 200}, ${Math.random() * 200})`,
+    ),
+  )
+
+  const clock = useClock()
+  clock.start()
+
+  const [cursor, setCursor] = createSignal<{ x: number; y: number }>()
+
+  const reducedCounter = createMemo(() => Math.floor(counter() / 200))
+
+  createEffect(() => console.log(reducedCounter()))
+
+  return (
+    <>
+      <Canvas
+        fill={fill()}
+        clock={clock.clock()}
+        style={{ width: '100%', height: '100%' }}
+        alpha
+        feedback
+        cursor="crosshair"
+        stats
+        onMouseMove={event => {
+          setCursor(event.position)
+          increment()
+        }}
+        onFrame={() => increment()}
+      >
+        <Show when={cursor()}>
+          <Smiley counter={reducedCounter()} position={cursor()!} />
+        </Show>
+      </Canvas>
+    </>
+  )
+}
+
+export default App
