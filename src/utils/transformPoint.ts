55b6d32087062ff0e9de445569bbc618a6429c61
bigmistqke <vandijckv@gmail.com>
2023-03-23 20:22:36 +0100

    remove variable declaration

--- src/utils/transformPoint.ts
+++ src/utils/transformPoint.ts
@@ -1,7 +1,4 @@
-import { createMemo } from 'solid-js'
 import { Position } from 'src/types'
 
-export default (position: Position, matrix: DOMMatrix) => {
-  const point = new DOMPoint(position.x, position.y)
-  return point.matrixTransform(matrix)
-}
+export default (position: Position, matrix: DOMMatrix) =>
+  new DOMPoint(position.x, position.y).matrixTransform(matrix)
