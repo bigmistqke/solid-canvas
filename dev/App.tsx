1a9f39d2b60286f317763c708fef620643b64059
bigmistqke <vandijckv@gmail.com>
2023-03-23 23:33:53 +0100

    Update example

--- dev/App.tsx
+++ dev/App.tsx
@@ -1,6 +1,6 @@
 import { Component, createMemo, createSignal, on, Show } from 'solid-js'
-import { Arc, Canvas, useCanvas, useClock } from 'src'
-import { Composite, Position } from 'src/types'
+import { Arc, Canvas, Image, useClock, Text } from 'src'
+import { Composite } from 'src/types'
 
 const randomColor = (alpha?: number) => ({
   h: Math.random() * 360,
@@ -9,46 +9,8 @@ const randomColor = (alpha?: number) => ({
   a: alpha ?? 0.5,
 })
 
-const Dot = (props: { switchColor: number; position: Position; switchScene: number }) => {
-  const context = useCanvas()
-  const [position, setPosition] = createSignal({ x: 0, y: 0 })
-
-  const delta = Math.random() * 10 + 20
-
-  context?.onFrame(() => {
-    setPosition({
-      x: props.position.x - radius() + Math.sin(performance.now() / (10 * delta) + delta) * 10,
-      y: props.position.y - radius() + Math.cos(performance.now() / (10 * delta) + delta) * 10,
-    })
-  })
-
-  const fill = createMemo(
-    on(
-      () => props.switchColor,
-      () => randomColor(Math.random() + 0.2),
-    ),
-  )
-
-  const radius = createMemo(
-    on(
-      () => props.switchScene,
-      () => 50 * Math.random() * 2 + 25,
-    ),
-  )
-  return (
-    <Arc
-      position={{ x: position().x, y: position().y }}
-      radius={radius()}
-      fill={fill()}
-      stroke="transparent"
-      draggable
-      pointerEvents={false}
-    />
-  )
-}
-
 const fxs: {
-  filter: string
+  filter?: string
   alpha: number
   composite?: Composite
   offset?: { x: number; y: number } | (() => { x: number; y: number })
@@ -69,7 +31,6 @@ const fxs: {
     },
   },
   {
-    filter: ``,
     alpha: 0.1,
     offset: {
       x: 0,
@@ -78,7 +39,6 @@ const fxs: {
     composite: 'lighten',
   },
   {
-    filter: ``,
     alpha: 0.75,
     offset: () => ({
       x: -1 * Math.sin(performance.now() / 5000),
@@ -99,7 +59,7 @@ const App: Component = () => {
 
   const switchColor = createMemo(() => Math.floor(clock.clock() / 200))
 
-  const incrementSwitchScene = () => setSwitchScene(c => (c + 1) % fxs.length)
+  const incrementSwitchScene = () => setSwitchScene(c => c + 1)
 
   window.addEventListener('keydown', event => {
     if (event.code === 'Space') {
@@ -111,13 +71,12 @@ const App: Component = () => {
   const feedback = (ctx: CanvasRenderingContext2D) => {
     const bitmap = createImageBitmap(ctx.canvas)
     bitmap.then(bitmap => {
-      const fx = fxs[switchScene()]
+      const fx = fxs[switchScene() % fxs.length]
       ctx.restore()
       ctx.save()
       ctx.globalAlpha = fx?.alpha ?? 1
       if (fx?.composite) ctx.globalCompositeOperation = fx?.composite
-
-      if (fx?.filter) ctx.filter = fx?.filter ?? ''
+      if (fx?.filter) ctx.filter = fx.filter
 
       const offset = typeof fx?.offset === 'function' ? fx?.offset() : fx?.offset
       ctx.drawImage(bitmap, offset?.x ?? 0, offset?.y ?? 0, ctx.canvas.width, ctx.canvas.height)
@@ -136,6 +95,21 @@ const App: Component = () => {
     })
   }
 
+  const image = createMemo(
+    () =>
+      [
+        'https://thumbs.dreamstime.com/b/rainbow-sky-14537169.jpg',
+        'https://thumbs.dreamstime.com/b/rainbow-blue-sky-21447453.jpg',
+        'https://www.ageuk.org.uk/bp-assets/globalassets/east-grinstead--district/images/home-page/rainbow.png',
+      ][switchScene() % 3],
+  )
+
+  const position = createMemo(
+    on(switchColor, () => ({
+      x: (Math.random() * window.innerWidth) / 2,
+      y: (Math.random() * window.innerHeight) / 2,
+    })),
+  )
   return (
     <>
       <Canvas
@@ -144,13 +118,45 @@ const App: Component = () => {
         style={{ width: '100%', height: '100%' }}
         alpha
         feedback={feedback}
-        cursor="crosshair"
+        cursor="none"
         onMouseMove={event => setCursor(event.position)}
         onMouseDown={event => incrementSwitchScene()}
       >
+        <Text
+          text="link"
+          rounded={5}
+          padding={10}
+          fill={{ r: 0, g: 0, b: 250 }}
+          background="white"
+          size={15}
+          hover={{ background: 'black', fill: 'white' }}
+          position={{ x: position().x, y: position().y + 300 }}
+          onMouseDown={() =>
+            window.open(
+              [
+                'https://thumbs.dreamstime.com/b/rainbow-sky-14537169.jpg',
+                'https://thumbs.dreamstime.com/b/rainbow-blue-sky-21447453.jpg',
+                'https://www.ageuk.org.uk/bp-assets/globalassets/east-grinstead--district/images/home-page/rainbow.png',
+              ][switchScene() % 3]!,
+            )
+          }
+        />
         <Show when={cursor()}>
-          <Dot switchColor={switchColor()} switchScene={switchScene()} position={cursor()!} />
+          <Image
+            pointerEvents={false}
+            dimensions={{ width: 100, height: 100 }}
+            position={{ x: cursor()!.x - 50, y: cursor()!.y - 50 }}
+            image={image()!}
+            clip={() => <Arc radius={50} />}
+          />
         </Show>
+        <Image
+          pointerEvents={false}
+          dimensions={{ width: 500, height: 300 }}
+          position={position()}
+          image={image()!}
+          opacity={0.2}
+        />
       </Canvas>
     </>
   )
