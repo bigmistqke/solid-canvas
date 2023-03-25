1597ec24efe1b3fe85594685bd43c55f08410838
bigmistqke <vandijckv@gmail.com>
2023-03-25 16:45:39 +0100

    `addVectors` take any number of args

--- src/utils/addVectors.ts
+++ src/utils/addVectors.ts
@@ -1,12 +1,17 @@
 import { Position } from 'src/types'
 
-const addPositions = (...args: Position[]) => {
-  const result = { x: 0, y: 0 }
+const addPositions = <T extends Position | undefined>(...args: T[]) => {
+  let result!: T
+  let arg: Position | undefined
 
-  args.forEach(arg => {
-    result.x += arg.x
-    result.y += arg.y
-  })
+  for (arg of args) {
+    if (!arg) continue
+    if (!result) result = { ...arg } as T
+    else {
+      result.x += arg.x
+      result.y += arg.y
+    }
+  }
 
   return result
 }
