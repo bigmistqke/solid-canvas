43e5774ca3691c196b268e8610d0a6ddb920cd41
bigmistqke <vandijckv@gmail.com>
2023-03-27 20:52:41 +0200

    visualize bezierpath as svg-element for debugging purposes

---
+++ src/utils/useDebugSvg.tsx
@@ -0,0 +1,24 @@
+import { Accessor, createSignal } from 'solid-js'
+import { Portal } from 'solid-js/web'
+
+export default () => {
+  const [svg, setSvg] = createSignal('')
+  const portal = (
+    <Portal>
+      <svg
+        style={{
+          position: 'absolute',
+          top: '0px',
+          left: '0px',
+          'z-index': 1,
+          stroke: 'black',
+          fill: 'transparent',
+        }}
+        height={1000}
+      >
+        <path d={svg()} />
+      </svg>
+    </Portal>
+  )
+  return setSvg
+}
