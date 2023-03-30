12e221c9d91324c8458a482fbdf0b78d0160b030
bigmistqke <vandijckv@gmail.com>
2023-03-30 22:50:55 +0200

    `ClickStyle` controller for demo-purposes

---
+++ src/controllers/ClickStyle.ts
@@ -0,0 +1,28 @@
+import { createSignal } from 'solid-js'
+import { ExtendedColor } from 'src/types'
+import { mergeGetters } from 'src/utils/mergeGetters'
+import { createController } from './createController'
+
+type HoverOptions = {
+  active?: boolean
+  stroke?: ExtendedColor
+  fill?: ExtendedColor
+}
+const ClickStyle = createController<HoverOptions>((props, events, options) => {
+  const [selected, setSelected] = createSignal(false)
+
+  events.onMouseDown(() => setSelected(true))
+  events.onMouseUp(() => setSelected(false))
+
+  return () =>
+    mergeGetters(props(), {
+      get stroke() {
+        return selected() ? options.stroke : props().stroke
+      },
+      get fill() {
+        return selected() ? options.fill : props().fill
+      },
+    })
+})
+
+export { ClickStyle }
