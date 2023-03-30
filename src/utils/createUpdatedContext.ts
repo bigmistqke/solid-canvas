c96c33f7babb61453af6b4257a9e85ff56634d9a
bigmistqke <vandijckv@gmail.com>
2023-03-30 15:51:16 +0200

    abstract updating origin context into `createUpdatedContext`

---
+++ src/utils/createUpdatedContext.ts
@@ -0,0 +1,20 @@
+import {
+  InternalContextType,
+  useInternalContext,
+} from 'src/context/InternalContext'
+import { ResolvedShape2DProps } from 'src/types'
+import { mergeGetters } from './mergeGetters'
+
+const createUpdatedContext = (props: () => ResolvedShape2DProps) => {
+  const internalContext = useInternalContext()
+  return mergeGetters(internalContext, {
+    get origin() {
+      return {
+        x: (internalContext?.origin.x ?? 0) + props().position.x,
+        y: (internalContext?.origin.y ?? 0) + props().position.y,
+      }
+    },
+  }) as InternalContextType
+}
+
+export { createUpdatedContext }
