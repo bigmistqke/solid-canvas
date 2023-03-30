19d58435abcaea44130efa90dbe1d52f971319fd
bigmistqke <vandijckv@gmail.com>
2023-03-30 15:51:39 +0200

    abstract handling of `children` into `createParenthood`

---
+++ src/utils/createParenthood.ts
@@ -0,0 +1,103 @@
+import { resolveTokens, TokenElement } from '@solid-primitives/jsx-tokenizer'
+import { Accessor, createEffect, JSX } from 'solid-js'
+import {
+  InternalContext,
+  InternalContextType,
+} from 'src/context/InternalContext'
+import { ControllerEvents } from 'src/controllers/.'
+
+import { CanvasToken, parser } from 'src/parser'
+import {
+  CanvasMouseEvent,
+  Composite,
+  ExtendedColor,
+  Position,
+  Shape2DProps,
+} from 'src/types'
+import forEachReversed from 'src/utils/forEachReversed'
+import { resolveExtendedColor } from 'src/utils/resolveColor'
+import withContext from 'src/utils/withContext'
+
+/**
+ * Object2Ds (and clips) the component's children
+ */
+
+export type Object2DProps = {
+  /**
+   * Defaults to { x: 0, y: 0}
+   */
+  position?: Position
+  children?: JSX.Element | JSX.Element[]
+  opacity?: number
+  fill?: ExtendedColor
+  composite?: Composite
+  clip?: Accessor<JSX.Element | JSX.Element[]>
+  draggable?: boolean | 'controlled'
+  controllers?: ((props: Object2DProps, events: ControllerEvents) => any)[]
+  onMouseDown?:
+    | ((event: CanvasMouseEvent) => void)
+    | ((event: CanvasMouseEvent) => void)[]
+  onMouseUp?:
+    | ((event: CanvasMouseEvent) => void)
+    | ((event: CanvasMouseEvent) => void)[]
+  onMouseMove?:
+    | ((event: CanvasMouseEvent) => void)
+    | ((event: CanvasMouseEvent) => void)[]
+  onMouseEnter?:
+    | ((event: CanvasMouseEvent) => void)
+    | ((event: CanvasMouseEvent) => void)[]
+  onMouseLeave?:
+    | ((event: CanvasMouseEvent) => void)
+    | ((event: CanvasMouseEvent) => void)[]
+}
+
+function createParenthood<T>(
+  props: () => Shape2DProps,
+  context?: InternalContextType,
+) {
+  const tokens = resolveTokens(
+    parser,
+    withContext(() => props().children, InternalContext, context),
+  )
+
+  const render = (ctx: CanvasRenderingContext2D) => {
+    forEachReversed(tokens(), ({ data }) => {
+      if ('render' in data) data.render?.(ctx)
+    })
+    forEachReversed(tokens(), ({ data }) => {
+      if ('debug' in data) data.debug(ctx)
+    })
+  }
+
+  const hitTest = (event: CanvasMouseEvent) => {
+    let result: TokenElement<CanvasToken>[] = []
+    forEachReversed(tokens(), token => {
+      if (!event.propagation) return
+      if ('hitTest' in token.data) {
+        const hit = token.data.hitTest(event)
+        if (hit) {
+          result.push(token)
+        }
+      }
+    })
+
+    const eventHandler = props()[event.type]
+
+    if (
+      event.target[0] === tokens()[tokens().length - 1]?.data &&
+      eventHandler
+    ) {
+      if (Array.isArray(eventHandler)) {
+        eventHandler.forEach(handler => handler(event))
+      } else {
+        eventHandler(event)
+      }
+    }
+
+    return false
+  }
+
+  return { render, hitTest }
+}
+
+export { createParenthood }
