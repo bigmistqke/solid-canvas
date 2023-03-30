0d5e5f114723a90b1cd6a7fbdbf3fb8b9beb4c8a
bigmistqke <vandijckv@gmail.com>
2023-03-30 19:51:25 +0200

    prevent error being thrown by providing fallback

--- src/utils/mergeGetters.ts
+++ src/utils/mergeGetters.ts
@@ -2,8 +2,8 @@ function mergeGetters<A, B>(a: A, b: B) {
   const result = Object.defineProperties(
     {},
     {
-      ...Object.getOwnPropertyDescriptors(a),
-      ...Object.getOwnPropertyDescriptors(b),
+      ...Object.getOwnPropertyDescriptors(a ?? {}),
+      ...Object.getOwnPropertyDescriptors(b ?? {}),
     },
   )
   return result as A & B
