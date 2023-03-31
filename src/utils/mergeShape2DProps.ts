cf13573c3513d72edf57bd67c938cc9910966c89
bigmistqke <vandijckv@gmail.com>
2023-03-31 18:41:13 +0200

    improve typing `mergeShape2DProps` will need some more thought later on

--- src/utils/mergeShape2DProps.ts
+++ src/utils/mergeShape2DProps.ts
@@ -2,10 +2,11 @@ import { mergeProps, splitProps } from 'solid-js'
 import { defaultShape2DProps } from 'src/defaultProps'
 import { Shape2DProps } from 'src/types'
 
+// TODO:   we have to think a bit more through how to type these arguments/returntype
 function mergeShape2DProps<
   T extends Record<string, unknown>,
   U extends { [K in keyof T]: T[K] },
->(props: Shape2DProps<T>, defaults: U) {
+>(props: Shape2DProps<T>, defaults: Partial<U>) {
   ;[, props] = splitProps(props, ['children'])
   return mergeProps({ ...defaultShape2DProps, ...defaults }, props)
 }
