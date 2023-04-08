d04743b3394676b2ee33eea1e3b44245f6e8c5a0
bigmistqke <vandijckv@gmail.com>
2023-04-08 03:53:29 +0200

    remove `Transition` from controllers

--- src/controllers/controllers.ts
+++ src/controllers/controllers.ts
@@ -5,7 +5,6 @@ export { CubicHandle, QuadraticHandle } from './Handle'
 export { Noop } from './Noop'
 export { ClickStyle } from './ClickStyle'
 export { Hover } from './Hover'
-export { Transition } from './Transition'
 
 export type ControllerEvents = {
   [K in CanvasMouseEventTypes]: (event: CanvasMouseEvent) => void
