e1e32ff60f69264ad32de0442ea9d36abbf9eec1
bigmistqke <vandijckv@gmail.com>
2023-04-11 01:13:44 +0200

    cache `props.style` in `renderPath` A big amount of time (10%) was spent in getting styles in `Hover`. Caching the result up front in a mutated `style` variable brings the cost of accessing props.style down to 2%.

--- src/utils/renderPath.ts
+++ src/utils/renderPath.ts
@@ -2,43 +2,40 @@ import { InternalContextType } from 'src/context/InternalContext'
 import { ResolvedShape2DProps } from 'src/types'
 import { resolveColor, resolveExtendedColor } from './resolveColor'
 
+let style
 export default (
   context: InternalContextType,
   props: ResolvedShape2DProps<any>,
   path: Path2D,
 ) => {
+  style = props.style
   context.ctx.save()
 
   // NOTE:  it would be more performant if we would compose commands from the styles with mapArray
   //        and forEach execute them, instead of doing checks on each renderPath.
 
-  if (props.style.shadow) {
-    context.ctx.shadowBlur = props.style.shadow.blur ?? 0
-    context.ctx.shadowOffsetX = props.style.shadow.offset?.x ?? 0
-    context.ctx.shadowOffsetY = props.style.shadow.offset?.y ?? 0
+  if (style.shadow) {
+    context.ctx.shadowBlur = style.shadow.blur ?? 0
+    context.ctx.shadowOffsetX = style.shadow.offset?.x ?? 0
+    context.ctx.shadowOffsetY = style.shadow.offset?.y ?? 0
     context.ctx.shadowColor =
-      resolveColor(props.style.shadow.color ?? 'black') ?? 'black'
+      resolveColor(style.shadow.color ?? 'black') ?? 'black'
   }
-  if (props.style.composite)
-    context.ctx.globalCompositeOperation = props.style.composite
-  if (props.style.opacity) context.ctx.globalAlpha = props.style.opacity
+  if (style.composite) context.ctx.globalCompositeOperation = style.composite
+  if (style.opacity) context.ctx.globalAlpha = style.opacity
 
-  if (props.style.fill) {
-    context.ctx.fillStyle =
-      resolveExtendedColor(props.style.fill) ?? 'transparent'
+  if (style.fill) {
+    context.ctx.fillStyle = resolveExtendedColor(style.fill) ?? 'transparent'
     context.ctx.fill(path)
   }
-  if (props.style.stroke && props.style.stroke !== 'transparent') {
-    if (props.style.lineWidth) context.ctx.lineWidth = props.style.lineWidth
-    if (props.style.miterLimit) context.ctx.miterLimit = props.style.miterLimit
-    if (props.style.lineJoin)
-      context.ctx.lineJoin = props.style.lineJoin ?? 'bevel'
-    if (context.ctx.lineCap)
-      context.ctx.lineCap = props.style.lineCap ?? 'round'
-    if (props.style.lineDash) context.ctx.setLineDash(props.style.lineDash)
+  if (style.stroke && style.stroke !== 'transparent') {
+    if (style.lineWidth) context.ctx.lineWidth = style.lineWidth
+    if (style.miterLimit) context.ctx.miterLimit = style.miterLimit
+    if (style.lineJoin) context.ctx.lineJoin = style.lineJoin ?? 'bevel'
+    if (context.ctx.lineCap) context.ctx.lineCap = style.lineCap ?? 'round'
+    if (style.lineDash) context.ctx.setLineDash(style.lineDash)
 
-    context.ctx.strokeStyle =
-      resolveExtendedColor(props.style.stroke) ?? 'black'
+    context.ctx.strokeStyle = resolveExtendedColor(style.stroke) ?? 'black'
     context.ctx.stroke(path)
   }
 
